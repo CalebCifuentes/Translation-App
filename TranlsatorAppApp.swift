7e37a57611305734d97b3c10d4f2ae8dbf2881ab
NoahKifle <156949804+NoahKifle@users.noreply.github.com>
2026-04-25 22:08:04 -0500

    Automatic Code from Xcode

---
+++ TranlsatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslatorAppApp.swift
+//  TranslatorApp
+//
+//  Created by Noah Kifle on 3/10/26.
+//
+
+import SwiftUI
+
+@main
+struct TranslatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
